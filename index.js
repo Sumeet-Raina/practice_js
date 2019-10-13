697c28902a4f61683c3ad23877643c9f7eafca33
sumeet978 <searchsumeet@gmail.com>
2019-10-13 18:06:43 +0100

    understanding data types

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("Hello World")
+let name = 'Sumeet'; //String literal
+let age = 32; // Number literal
+let isApproved = true; //Boolean literal
+let foo; // foo does not exist. It is not defined and has never been initialized
+let bar = null; // bar is known to exist but it has no type or value
+console.log(name);
+console.log(age);
+console.log(isApproved);
+console.log(foo);
+console.log(bar);
